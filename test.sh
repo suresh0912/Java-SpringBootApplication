71ecd4df852e402218927ce258829df491b14ea3
suresh0912 <suresh.perlcgi@gmail.com>
2020-01-22 12:01:13 +0530

    test

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-date
+sudo apt install software-properties-common
+sudo apt-add-repository ppa:webupd8team/java
+sudo apt update
+sudo apt install oracle-java8-installer
+java -version
 
